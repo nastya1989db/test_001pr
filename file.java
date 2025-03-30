05d5391f4e17eaaaefdb698f55d193d88ad97a82
nastya1989db <nastenkaya1989@gmail.com>
2025-03-30 12:06:42 +0000

    Different fix in the three files in repo001

--- file.java
+++ file.java
@@ -1,5 +1,5 @@
 public class Invitation {
     public static void main(String[] args)  {
-                System.out.print("Hello people");
+                System.out.print("My name is Gosha. I am 22 years old. My height 182 sm.");
             }
         }
